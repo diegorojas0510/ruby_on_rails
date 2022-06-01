5ab48a722e1fdb4975e5e9c9dc19ea4d0d8082e6
diegorojas0510 <diegorojas.0510@gmail.com>
2022-05-31 21:11:53 -0500

    ejercicios 31-05-2022

--- clase_repaso/clase_3/Ej_classes_1_2_3/contrasenas.rb
+++ clase_repaso/clase_3/Ej_classes_1_2_3/contrasenas.rb
@@ -17,6 +17,7 @@
 # 2-el método recibe una cadena como argumento
 # 3-validar el método que convierte de mayusculas a minusculas
 # 4-validar el método en ruby que quita los espacios en blanco
+# 4.1 convertir la cadena a un array
 # 5-crear un ciclo para reemplazar las letras de la siguiente manera:
 # * Se reemplaza la `a` por `4`.
 # * Se reemplaza la `e` por `3`.
@@ -24,21 +25,15 @@
 # * Se reemplaza la `o` por `0`.
 
 def pass(password_usr)
-  # password_usr.downcase.gsub(/\s+/, '')
-  password_usr.each do |character|
-    p character.downcase.gsub(/\s+/, '')
-    # if character == ""
-    #   ""
-    # elsif character == "a"
-    #   4
-    # elsif character == "e"
-    #   3
-    # elsif character == "i"
-    #   1
-    # elsif character == "o"
-    #   0
-    # end
+  pass = password_usr.downcase.gsub(/\s+/, '').chars
+  #p pass
+  pass.each do |character|
+    character.replace '4' if character == 'a'
+    character.replace '3' if character == 'e'
+    character.replace '1' if character == 'i'
+    character.replace '0' if character == 'o'
   end
-
+  pass.join('')
 end
-p pass('hOlA como estas')
+p pass('hola')
+p pass('esta es una prueba')
