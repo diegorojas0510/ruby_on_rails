afae103fca88b2b1dcfe8eb8ec4b73752454501b
diegorojas0510 <diegorojas.0510@gmail.com>
2022-06-06 18:54:53 -0500

    Rails green-baazar add bootstrap button

--- clase_repaso/clase_4/Ej_classes_3_4/hashes.rb
+++ clase_repaso/clase_4/Ej_classes_3_4/hashes.rb
@@ -6,4 +6,22 @@
 # puts hash([34, 925, 322]) # {0=>34, 1=>925, 2=>322}
 # puts hash(["make", "it", "real"]) # {0=>"make", 1=>"it", 2=>"real"}
 # puts hash([]) # {}
+# pseudocodigo
+# 1- crear un metodo que reciba un arreglo
+# 2- crear un hash vacio
+# 3- iterar el arreglo
+# 4- asignar el valor del arreglo a la llave del hash
+# 5- retornar el hash
+# 6- imprimir el hash
+# 7- imprimir el hash con una llave y un valor
 
+def hash(arr)
+  hash = {}
+  arr.each_with_index do |val, index|
+    hash[index] = val
+  end
+  hash
+end
+
+p hash(%w[0 1 2 3 4])
+p hash(%w[34 925 322])
